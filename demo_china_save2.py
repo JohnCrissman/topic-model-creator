cd1df1ebff8800d974b3f22e001ef91fb2d079ba
John Crissman <JohnMCrissmanUSMC@gmail.com>
2020-02-07 21:44:14 -0600

    working on preparing data for classifiers

--- demo_china_save2.py
+++ demo_china_save2.py
@@ -1,3 +1,33 @@
 # demo_china_save2.py
 # merge excel files together into a pandas dataframe
 
+# first make j_testing.csv merge with m_testing.csv
+
+import csv
+import pandas as pd
+
+df_j = pd.read_excel('j_testing.xlsx')
+df_m = pd.read_excel('m_testing.xlsx')
+
+print("Here is df_j:")
+print(df_j)
+
+print("here is df_m:")
+print(df_m)
+
+df_j_m = df_j.join(df_m.set_index('record_id'), on='record_id')
+print(df_j_m)
+
+df_tracking_log = pd.read_excel('Tracking_Log_1-10_for_NEIU_excel.xlsx')
+df_demographics = pd.read_excel('PN_demographics_neiu.xlsx')
+print(df_tracking_log)
+print(df_demographics)
+df_all_INFO = df_tracking_log.merge(df_demographics, on='record_id', how='outer')
+print(df_all_INFO)
+# left.merge(right, on='key', how='outer')
+# df_all_INFO = df_tracking_log.join(df_demographics.set_index('record_id'), on='record_id')
+
+df_all_INFO.to_csv('df_all_INFO.csv', encoding='utf-8', index=False)
+
+
+
