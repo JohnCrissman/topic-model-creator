6ee177f05a503b24c846a75252ef03db1cf8b7a4
John Crissman <JohnMCrissmanUSMC@gmail.com>
2020-02-11 17:21:45 -0600

    working on demo_china_save2

--- demo_china_save2.py
+++ demo_china_save2.py
@@ -22,12 +22,15 @@ df_tracking_log = pd.read_excel('Tracking_Log_1-10_for_NEIU_excel.xlsx')
 df_demographics = pd.read_excel('PN_demographics_neiu.xlsx')
 print(df_tracking_log)
 print(df_demographics)
-df_all_INFO = df_tracking_log.merge(df_demographics, on='record_id', how='outer')
+# df_all_INFO = df_tracking_log.merge(df_demographics, on='record_id', how='outer')
+df_all_INFO = df_demographics.merge(df_tracking_log, on='record_id', how='inner')
+
+
+
+df_all_INFO = df_all_INFO.loc[:,'record_id':'navigation_comments1']
+
+df_all_INFO.columns = df_all_INFO.columns.str.replace('barrier1','Classification')
+
 print(df_all_INFO)
-# left.merge(right, on='key', how='outer')
-# df_all_INFO = df_tracking_log.join(df_demographics.set_index('record_id'), on='record_id')
-
 df_all_INFO.to_csv('df_all_INFO.csv', encoding='utf-8', index=False)
-
-
-
+# classifying_patients = ClassifierProcessor(df_tracking_log)
